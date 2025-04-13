227450b6f3dd26909607fba2d6bd84d10cb48850
Aisha <aishadn2017@gmail.com>
2025-04-14 01:40:38 +0800

    App

---
+++ src/com/javaconcepts/oop/inheritance/InheritanceApp.java
@@ -0,0 +1,53 @@
+package com.javaconcepts.oop.inheritance;
+
+import com.javaconcepts.encapsulation.Student;
+import com.javaconcepts.oop.inheritence.Bike;
+import com.javaconcepts.oop.inheritence.Boat;
+import com.javaconcepts.oop.inheritence.Car;
+import com.javaconcepts.oop.inheritence.Vehicle;
+
+import java.util.Scanner;
+
+public class InheritanceApp {
+
+    public static void main(String[] ard) {
+        //drive();
+
+        Student s1 = new Student();
+        s1.setName("Ali");
+        System.out.println(s1.getName());
+    }
+
+    public static void drive(){
+
+        Scanner scanner = new Scanner(System.in);
+        int input;
+        Vehicle v;
+
+        do {
+            System.out.println("Please enter value test: ");
+            input = scanner.nextInt();
+
+            System.out.println("Entered Value is: " + input);
+
+            if (input == 1) {
+                v = new Car();
+            } else if (input == 2) {
+                v = new Bike();
+            } else if (input == 3) {
+                v = new Boat();
+            } else {
+                v = new Vehicle();
+            }
+
+            startAnyEngine(v);
+
+        } while (input != 0);
+
+
+    }
+
+    public static void startAnyEngine (Vehicle v) {
+        v.startEngine();
+    }
+}
