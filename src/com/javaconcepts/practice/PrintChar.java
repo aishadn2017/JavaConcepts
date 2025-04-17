ee68180fa846cd2c1d061cc8cfe136801fe3648b
Aisha <aishadn2017@gmail.com>
2025-04-17 20:31:43 +0800

    PrintChar task

---
+++ src/com/javaconcepts/practice/PrintChar.java
@@ -0,0 +1,29 @@
+package com.javaconcepts.practice;
+
+public class PrintChar {
+
+
+    public static void main(String[] args) {
+
+
+        String strg = "Aisha";
+
+
+        for (int i = 0; i < strg.length(); i++) {
+
+            char ch = strg.charAt(i);
+            System.out.println(ch);
+
+        }
+
+
+
+    }
+
+
+}
+
+
+
+
+
